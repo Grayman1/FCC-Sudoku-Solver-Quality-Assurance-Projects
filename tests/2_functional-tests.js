cad04ee128b38595582a03aafd1bc950ccdb2778
Grayman1 <>
2021-05-05 02:56:56 +0000

    Add user stories, unit tests, functional tests to README. Add template for functional tests.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,5 +7,34 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
 
+  test("Test POST /api/books with title", function(done) {
+          chai
+            .request(server)
+            .post("/api/books")
+            .send({ title: "test title" })
+            .end((err, res) => {
+              bookId = res.body._id;
+              assert.equal(res.status, 200);
+              assert.equal(res.body.title, "test title");
+              done();
+            });
+        });
+
+        test("Test POST /api/books with no title given", function(done) {
+          chai
+            .request(server)
+            .post("/api/books")
+            .send({})
+            .end((err, res) => {
+              assert.equal(res.status, 200);
+              assert.equal(res.body, "missing required field title");
+              done();
+            });
+        });
+
+
+
+
+
 });
 
